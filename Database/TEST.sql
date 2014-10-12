ef0320c061264ee626d4b4362e14385c85e8f562
stesesia <sesia.stefano@gmail.com>
2014-10-12 10:18:10 +0200

    WeHaveCakeSubmission

---
+++ Database/TEST.sql
@@ -0,0 +1,33 @@
+INSERT INTO routes value('01:23:45:67:89:10','2014-10-12 09:00:00','Stefano',0);v
+INSERT INTO routes value('11:61:A1:B2:99:01','2014-10-12 09:30:00','Stefano',1);v
+INSERT INTO routes value('0A:83:1B:94:2C:A5','2014-10-12 10:00:00','Stefano',2);v
+INSERT INTO routes value('1B:94:2C:A5:3D:B6','2014-10-12 10:30:00','Stefano',3);v
+INSERT INTO routes value('2C:A5:3D:B6:4E:C7','2014-10-12 11:00:00','Stefano',4);v
+INSERT INTO routes value('3D:B6:4E:C7:5F:D8','2014-10-12 11:30:00','Stefano',5);v
+INSERT INTO routes value('4E:C7:5F:D8:61:E9','2014-10-12 12:00:00','Stefano',6);v
+INSERT INTO routes value('5F:D8:61:E9:72:FA','2014-10-12 12:30:00','Stefano',7);v
+INSERT INTO routes value('61:E9:72:FA:83:1B','2014-10-12 13:00:00','Stefano',8);v
+INSERT INTO routes value('72:FA:483:1B:94:2C','2014-10-12 13:30:00','Stefano',9);
+UPDATE user SET CurrentLocation='72:FA:483:1B:94:2C' WHERE Username='Stefano'; 
+INSERT INTO routes value('00:A1:B3:90:13:59','2014-10-12 09:00:00','Andrew',10);
+INSERT INTO routes value('22:72:B2:C3:10:02','2014-10-12 09:30:00','Andrew',11);
+INSERT INTO routes value('0A:83:1B:94:2C:A5','2014-10-12 10:00:00','Andrew',12);
+INSERT INTO routes value('1B:94:2C:A5:3D:B6','2014-10-12 10:30:00','Andrew',13);
+INSERT INTO routes value('2C:A5:3D:B6:4E:C7','2014-10-12 11:00:00','Andrew',14);
+INSERT INTO routes value('3D:B6:4E:C7:5F:D8','2014-10-12 11:30:00','Andrew',15);
+INSERT INTO routes value('4E:C7:5F:D8:61:E9','2014-10-12 12:00:00','Andrew',16);
+INSERT INTO routes value('5F:D8:61:E9:72:FA','2014-10-12 12:30:00','Andrew',17);
+INSERT INTO routes value('61:E9:72:FA:83:1B','2014-10-12 13:00:00','Andrew',18);
+INSERT INTO routes value('72:FA:483:1B:94:2C','2014-10-12 13:30:00','Andrew',19);
+UPDATE user SET CurrentLocation='72:FA:483:1B:94:2C' WHERE Username='Andrew'; 
+INSERT INTO routes value('01:23:45:67:89:10','2014-10-12 09:00:00','Seoras',20);
+INSERT INTO routes value('11:61:A1:B2:99:01','2014-10-12 09:30:00','Seoras',21);
+INSERT INTO routes value('4E:C7:5F:D8:61:E9','2014-10-12 10:00:00','Seoras',22);
+INSERT INTO routes value('00:13:A1:B2:C3:E4','2014-10-12 10:30:00','Seoras',23);
+INSERT INTO routes value('1B:94:2C:A5:3D:B6','2014-10-12 11:00:00','Seoras',24);
+INSERT INTO routes value('72:FA:483:1B:94:2C','2014-10-12 11:30:00','Seoras',25);
+INSERT INTO routes value('01:23:45:67:89:10','2014-10-12 12:00:00','Seoras',26);
+INSERT INTO routes value('11:61:A1:B2:99:01','2014-10-12 12:30:00','Seoras',27);
+INSERT INTO routes value('2C:A5:3D:B6:4E:C7','2014-10-12 13:00:00','Seoras',28);
+INSERT INTO routes value('01:23:45:67:89:10','2014-10-12 13:30:00','Seoras',29);
+UPDATE user SET CurrentLocation='01:23:45:67:89:10' WHERE Username='Seoras'; 
